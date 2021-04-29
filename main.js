53de3f342e39918a23de58594b80ee2d496c43d1
Arka Bansal <github.arkbansal@gmail.com>
2021-04-29 18:49:48 +0530

    .
    ..

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+video = "";
 function preload() {
-    //empty
+    video = createVideo('video.mp4');
+    video.hide();
 }
 function setup() {
-    //empty 2
+    canvas = createCanvas(480 , 380);
+    canvas.center();
 }
 function draw() {
-    //empty 3
+    image(video , 0, 0, 480 , 280);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
